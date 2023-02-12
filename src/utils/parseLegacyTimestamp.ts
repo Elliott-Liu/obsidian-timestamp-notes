dead9ef6644dff3cfa9f5705cbc774d3dc0e1bdc
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-02-12 01:28:29 +0000

    Create parseLegacyTimestamp function

---
+++ src/utils/parseLegacyTimestamp.ts
@@ -0,0 +1,27 @@
+import { CodeBlockProperties } from "./parseTimestamp";
+
+export function parseLegacyTimestamp(source: string) {
+	const properties: CodeBlockProperties = {
+		properties: {},
+		legacyMode: true,
+	};
+
+	// Match mm:ss or hh:mm:ss timestamp format
+	const regExp = /\d+:\d+:\d+|\d+:\d+/g;
+	const rows = source.split("\n").filter((row) => row.length > 0);
+	let firstMatchValue: string = "";
+	let alreadyMatched: boolean = false;
+
+	rows.forEach((row) => {
+		const match = row.match(regExp);
+
+		if (match && alreadyMatched === false) {
+			firstMatchValue = String(match[0]);
+			alreadyMatched = true;
+		}
+	});
+
+	properties.properties = { timestamp: firstMatchValue };
+	return properties;
+}
+
